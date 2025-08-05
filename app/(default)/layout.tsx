b4b2e735975318208389448cc0d45b726f3c887f
mikaelpackowski <mikael.packowski@gmail.com>
2025-08-05 14:50:33 +0200

    Update layout.tsx

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,12 +1,10 @@
-import type { ReactNode } from "react";
+// app/(default)/layout.tsx
+
+import { ReactNode } from "react";
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
 
-interface DefaultLayoutProps {
-  children: ReactNode;
-}
-
-export default function DefaultLayout({ children }: DefaultLayoutProps) {
+export default function DefaultLayout({ children }: { children: ReactNode }) {
   return (
     <main className="relative flex grow flex-col bg-white text-gray-900">
       <Header />
