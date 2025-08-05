e2a0ca256b18f2eb6b22e6d81023b5ad33adbf0d
mikaelpackowski <mikael.packowski@gmail.com>
2025-08-05 14:59:08 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,22 +18,22 @@ const inter = Inter({
 const nacelle = localFont({
   src: [
     {
-      path: "/fonts/nacelle-regular.woff2",
+      path: "/public/fonts/nacelle-regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/fonts/nacelle-italic.woff2",
+      path: "/public/fonts/nacelle-italic.woff2",
       weight: "400",
       style: "italic",
     },
     {
-      path: "/fonts/nacelle-semibold.woff2",
+      path: "/public/fonts/nacelle-semibold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "/fonts/nacelle-semibolditalic.woff2",
+      path: "/public/fonts/nacelle-semibolditalic.woff2",
       weight: "600",
       style: "italic",
     },
